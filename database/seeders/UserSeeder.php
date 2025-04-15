e1f80bc5c65019fcd4beeb8059699300bc10d9ec
tiagokochem <tiagok989@gmail.com>
2025-04-15 09:07:26 -0300

    refactor: clean up DatabaseSeeder and enhance UserSeeder with consistent user creation

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,23 +2,34 @@
 
 namespace Database\Seeders;
 
+
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run()
+    public function run(): void
     {
         User::create([
-            'name' => 'Admin',
-            'email' => 'admin@estoque.com',
+            'name' => 'Admin User',
+            'email' => 'admin@teste.com',
             'password' => Hash::make('password'),
             'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Operador User',
+            'email' => 'operador@teste.com',
+            'password' => Hash::make('password'),
+            'role' => 'operador',
+        ]);
+
+        User::create([
+            'name' => 'Usuário Comum',
+            'email' => 'usuario@teste.com',
+            'password' => Hash::make('password'),
+            'role' => 'usuario',
         ]);
     }
 }
